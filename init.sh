4f4285c74d9514f8cff32f89a229fdf657a4bf43
ej <phelpse3@gmail.com>
2020-01-28 15:59:36 -0500

    changes from linux mint laptop

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-ln .vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+Ln lvimrc ~/.vimrc
 ln .tmux.conf ~/.tmux.conf
 # Results in a symbolic link loop??
 #ln -s .vim ~/.vim
