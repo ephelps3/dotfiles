64500c6ac32ec70d5521feabdcbbf109e98e885f
ej <phelpse3@gmail.com>
2020-02-15 09:38:05 -0500

    Adding all of the i3 config files

--- init.sh
+++ init.sh
@@ -5,3 +5,7 @@ Ln lvimrc ~/.vimrc
 ln .tmux.conf ~/.tmux.conf
 # Results in a symbolic link loop??
 #ln -s .vim ~/.vim
+
+# i3 install scripts on Mint
+#git clone https://github.com/erikdubois/i3-installation-on-latest-linux-mint
+
